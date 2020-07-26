1f64d7012583c063b7d4d60d38f8a7d1628fc194
ALEX SMOTRITSKY <alex.smotritsky@gmail.com>
2020-07-26 13:27:47 -0400

    incorrect_answers

--- src/client/Quiz.tsx
+++ src/client/Quiz.tsx
@@ -3,7 +3,12 @@ import axios from 'axios';
 import { cleanString } from '../util';
 
 function Quiz() {
-  const [data, setData] = useState([{category:'', question: ''}]);
+  const [data, setData] = useState([{category:'', 
+    question: '', 
+    correct_answer: '',
+    difficulty: '',
+    incorrect_answers: [],
+    type: ''}]);
   
   useEffect(() => {
     const fetchData = async () => {
@@ -17,11 +22,17 @@ function Quiz() {
 
   return (
     <div className="App">
-      {data.map((value, index) => {
+      {data && data.map((value, index) => {
         return (
           <div key={index}>
             question: {`${cleanString(value.question)}`}
             <p>category: {value.category}</p>
+            <p>correct answer: {value.correct_answer}</p>
+            <p>incorrect answers:</p>
+            {Array(value.incorrect_answers).map((answer, i) => { return (<p key={i}>{answer}</p>)})}
+            {/*value && value.incorrect_answers.map((answer, i) => {
+              return (<p key={i}>{answer}</p>)
+            })*/}
           </div>
         )
       })}
