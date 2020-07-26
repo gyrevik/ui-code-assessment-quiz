00b6166bb7749cf5e26cfd99109d66e6db7eac55
ALEX SMOTRITSKY <alex.smotritsky@gmail.com>
2020-07-26 11:50:08 -0400

    apostrophe

--- src/const.tsx
+++ src/const.tsx
@@ -1,2 +1,3 @@
 export const MIDDLE_DOT    = '\u0057';
 export const DOUBLE_QUOTE  = '\u0022';
+export const APOSTROPHE    = '\u0027';
--- src/util.tsx
+++ src/util.tsx
@@ -1,6 +1,7 @@
-import { DOUBLE_QUOTE } from './const'
+import { DOUBLE_QUOTE, APOSTROPHE } from './const'
 
 export function cleanString(str: String) {
     str = str.replace(/&quot;/gi, DOUBLE_QUOTE)
+    str = str.replace(/&#039;/gi, APOSTROPHE)
     return str;
 }
